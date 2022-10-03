5c19efe1b1308baabe620403a7ddd02f1fe02b7e
Eugene1337 <zhenya777pinsk@gmail.com>
2022-10-03 21:04:04 +0200

    add admin panel layout and refactor

---
+++ Gomar/Controllers/AdminController.cs
@@ -0,0 +1,53 @@
+﻿using Gomar.Models;
+using Gomar.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gomar.Controllers
+{
+    public class AdminController : Controller
+    {
+        UserManager _userManager;
+
+        public AdminController(UserManager userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Main()
+        {
+            return View();
+        }
+
+        [Route("Admin")]
+        public IActionResult LogIn()
+        {
+            return View();
+        }
+
+        [Route("Admin")]
+        [HttpPost]
+        public async Task<IActionResult> LogInAsync(LogInViewModel form)
+        {
+            if (!ModelState.IsValid)
+                return View(form);
+            try
+            {
+                await _userManager.SignIn(this.HttpContext, form.Email, form.Password);
+                return RedirectToAction("Main");
+            }
+            catch (Exception ex)
+            {
+                ViewData["ValidationSummary"] = ex.Message;
+                return View(form);
+            }
+        }
+
+        [Route("Logout")]
+        public IActionResult Logout()
+        {
+            _userManager.SignOut(this.HttpContext);
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
--- Gomar/Program.cs
+++ Gomar/Program.cs
@@ -49,5 +49,4 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-
 app.Run();
